6626ce53e8ad32931b687d40a34b816a5ed1e899
Marcin Górski <gorskimarcin96@gmail.com>
2025-06-17 23:17:24 +0200

    Blocked external users from connecting to the app.

--- coaster/app/Config/Filters.php
+++ coaster/app/Config/Filters.php
@@ -2,6 +2,7 @@
 
 namespace Config;
 
+use App\Filters\AccessDevelopmentByIPFilter;
 use CodeIgniter\Config\Filters as BaseFilters;
 use CodeIgniter\Filters\Cors;
 use CodeIgniter\Filters\CSRF;
@@ -34,6 +35,7 @@ class Filters extends BaseFilters
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
+        'accessdevelopmentbyip' => AccessDevelopmentByIPFilter::class,
     ];
 
     /**
@@ -69,14 +71,9 @@ class Filters extends BaseFilters
      */
     public array $globals = [
         'before' => [
-            // 'honeypot',
-            // 'csrf',
-            // 'invalidchars',
+            'accessdevelopmentbyip',
         ],
-        'after' => [
-            // 'honeypot',
-            // 'secureheaders',
-        ],
+        'after' => [],
     ];
 
     /**
---
+++ coaster/app/Filters/AccessDevelopmentByIPFilter.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\Response;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+
+final readonly class AccessDevelopmentByIPFilter implements FilterInterface
+{
+    private const ALLOWED_IPS = ['127.0.0.1'];
+
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        if (
+            env('CI_ENVIRONMENT') === 'development'
+            && !in_array($request->getIPAddress(), self::ALLOWED_IPS, true)
+        ) {
+            log_message(
+                'info',
+                sprintf('User with IP address %s tries to connect to the application.', $request->getIPAddress()),
+            );
+
+            /** @var Response $response */
+            $response = service('response');
+
+            return $response->setStatusCode(403)->setJSON(['error' => 'Forbidden']);
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null): void
+    {
+    }
+}
